12db56b58ca9585d34df30dc25ee140a4a567157
kyalo-vibes <kevin.kioko4@gmail.com>
2024-09-04 08:19:10 +0300

    Finished OOP 6

--- src/main/java/org/example/cloning/Human.java
+++ src/main/java/org/example/cloning/Human.java
@@ -3,18 +3,35 @@ package org.example.cloning;
 public class Human implements Cloneable {
     int age;
     String name;
+    int[] arr;
 
     public Human(Human other) {
         this.age = other.age;
         this.name = other.name;
+        this.arr = other.arr;
     }
 
     public Human(int age, String name) {
         this.age = age;
         this.name = name;
+        this.arr = new int[]{1, 2, 3};
     }
 
+//    public Object clone() throws CloneNotSupportedException {
+//        // this is shallow copying
+//        return super.clone();
+//    }
+
     public Object clone() throws CloneNotSupportedException {
-        return super.clone();
+        // this is shallow copying
+        Human twin = (Human) super.clone();
+
+        // this is deep copying
+        twin.arr = new int[twin.arr.length];
+        for (int i = 0; i < twin.arr.length; i++) {
+            twin.arr[i] = this.arr[i];
+        }
+        return twin;
     }
+    
 }
--- src/main/java/org/example/cloning/Main.java
+++ src/main/java/org/example/cloning/Main.java
@@ -1,16 +1,24 @@
 package org.example.cloning;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
         Human kyalo = new Human(54, "Kyalo");
         Human dan = new Human(kyalo);
         System.out.println(dan.name);
         dan.name = "D-man";
+        dan.arr[0] = 17;
         System.out.println(dan.name);
         System.out.println(kyalo.name);
+        System.out.println(Arrays.toString(dan.arr));
+        System.out.println(Arrays.toString(kyalo.arr));
 
         // Object cloning
         Human twin = (Human) dan.clone();
+        twin.arr[0] = 250;
         System.out.println(twin.name);
+        System.out.println(Arrays.toString(twin.arr));
+        System.out.println(Arrays.toString(dan.arr));
     }
 }
